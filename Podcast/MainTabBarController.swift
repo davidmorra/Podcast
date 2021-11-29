59fec06563c552332ceb0c720b83331d50362448
davidmorra <motogge@gmail.com>
2021-11-29 20:21:29 +0400

    Initial Commit

---
+++ Podcast/MainTabBarController.swift
@@ -0,0 +1,49 @@
+//
+//  MainTabBarController.swift
+//  Podcast
+//
+//  Created by Macbook on 11/29/21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        tabBar.tintColor = .purple
+        
+        setUpViewControllers()
+    }
+    
+    // MARK: - Set Up View Controllers
+    func setUpViewControllers() {
+        viewControllers = [
+            setUpNavigationControllers(with: ViewController(), title: "Favorites", image: "play.circle.fill"),
+            setUpNavigationControllers(with: ViewController(), title: "Search", image: "magnifyingglass"),
+            setUpNavigationControllers(with: ViewController(), title: "Donwloads", image: "rectangle.stack.fill")
+        ]
+    }
+    
+    // MARK: - Helper Functions
+    fileprivate func setUpNavigationControllers(with rootviewController: UIViewController, title: String, image: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: rootviewController)
+        rootviewController.navigationItem.title = title
+        
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: image)
+        navController.title = title
+        
+        return navController
+    }
+    
+    
+
+
+    
+}
---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by Macbook on 11/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
